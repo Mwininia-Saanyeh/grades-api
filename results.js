411614ffda79ac30efe2d7eeddd786ef868e2878
Mwininia-Saanyeh <ramoakidas@gmail.com>
2022-09-01 14:06:01 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results = [{ name: "Sadika Bint Omar", class:"final year", grade: "8"},
+                 { name: "Dong Leticia", class:"final year", grade: "8"},
+                 { name: "Orison Mustapha", class:"final year", grade: "9"},
+                 { name: "Adams Adinatu", class:"final year", grade: "11"},
+                 { name: "Ali Mohammed", class:"final year", grade: "12"},
+                 { name: "Samsideen Naeem", class:"final year", grade: "10"},
+]
+
+module.exports=results
